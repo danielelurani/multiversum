3d1f7db3583ef9f0208fd5e3e0d5a98e6ef66b1e
Daniele Lurani <47564259+danielelurani@users.noreply.github.com>
2023-05-23 12:06:08 +0200

    update: animazioni morte zombie

--- Assets/Scripts/ZombieWalkerHealth.cs
+++ Assets/Scripts/ZombieWalkerHealth.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class ZombieWalkerHealth : MonoBehaviour
 {
 
-    public float maxHealth = 50f;
+    public float maxHealth = 100f;
 
     [HideInInspector]
     public float currentHealth;
+
+    private Animator animator;
+    private UnityEngine.AI.NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
 
     public void TakeDamage(float amount){
@@ -21,11 +26,14 @@ public class ZombieWalkerHealth : MonoBehaviour
         currentHealth -= amount;
 
         if(currentHealth <= 0.0f){
+
+            animator.SetBool("isDead", true);
+            agent.speed = 0.0f;
             Die();
         }
     }
 
     public void Die(){
-        Destroy(gameObject);
+        Destroy(gameObject, 5.0f);
     }
 }
