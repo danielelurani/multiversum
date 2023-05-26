4b0d23ba4e58b06dcbf896e970e12ec3c5586d91
GhiaDx <126504730+GhiaDx@users.noreply.github.com>
2023-05-26 18:24:19 +0200

    Creazione schermata di Game Over

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CharacterStats : MonoBehaviour
 
@@ -24,7 +25,7 @@ public class CharacterStats : MonoBehaviour
 
     void Update()
     {
-
+        CheckHealth();
     }
 
     public void CheckHealth()
@@ -33,6 +34,7 @@ public class CharacterStats : MonoBehaviour
         {
             health = 0;
             isDead = true;
+            SceneManager.LoadScene("GameOverScene");
 
         }
 
