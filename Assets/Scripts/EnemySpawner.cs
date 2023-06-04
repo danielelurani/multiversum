b513b9a6c44f3068067a0e0f08518e9c253ac732
Daniele Lurani <47564259+danielelurani@users.noreply.github.com>
2023-06-05 00:41:21 +0200

    Fix spawn non funzionanti

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -9,6 +9,12 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private GameObject zombieTank;
     [SerializeField] private float interval = 1f;
 
+    private int random = 1;
+    private GameObject spwn1;
+    private GameObject spwn2;
+    private GameObject spwn3;
+    private GameObject spwn4;
+
     private int baseZombieWalkerCount = 5;
     private int baseZombieRunnerCount = 2;
     private int baseZombieTankCount = 1;
@@ -20,6 +26,11 @@ public class EnemySpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // trova gli spawn
+        spwn1 = GameObject.Find("Spawn1");
+        spwn2 = GameObject.Find("Spawn2");
+        spwn3 = GameObject.Find("Spawn3");
+        spwn4 = GameObject.Find("Spawn4");
 
         // carica gli assets necessari per gli zombie
         zombieWalker = Resources.Load<GameObject>("Prefabs/ZombieWalker");
@@ -31,12 +42,32 @@ public class EnemySpawner : MonoBehaviour
         StartCoroutine(spawnZT(interval, zombieTank));
     }
 
+    void Update() {
+        random = Random.Range(1,5);
+    }
+
     private IEnumerator spawnZW(float interval, GameObject enemyType){
 
         while(zombieWalkerCount < baseZombieWalkerCount * GameManager.currentWave){
 
             yield return new WaitForSeconds(interval);
-            GameObject newEnemy = Instantiate(enemyType, transform.position, Quaternion.identity);
+
+            if(random == 1){
+                GameObject newEnemy = Instantiate(enemyType, spwn1.transform.position, Quaternion.identity);
+            }
+                
+            if(random == 2){
+                GameObject newEnemy = Instantiate(enemyType, spwn2.transform.position, Quaternion.identity);
+            }
+
+            if(random == 3){
+                GameObject newEnemy = Instantiate(enemyType, spwn3.transform.position, Quaternion.identity);
+            }
+
+            if(random == 4){
+                GameObject newEnemy = Instantiate(enemyType, spwn4.transform.position, Quaternion.identity);
+            }
+                
 
             zombieWalkerCount ++;
             GameManager.zombiesAlive ++;
@@ -48,7 +79,22 @@ public class EnemySpawner : MonoBehaviour
         while(zombieRunnerCount < baseZombieRunnerCount * GameManager.currentWave){
 
             yield return new WaitForSeconds(interval);
-            GameObject newEnemy = Instantiate(enemyType, transform.position, Quaternion.identity);
+
+            if(random == 1){
+                GameObject newEnemy = Instantiate(enemyType, spwn1.transform.position, Quaternion.identity);
+            }
+                
+            if(random == 2){
+                GameObject newEnemy = Instantiate(enemyType, spwn2.transform.position, Quaternion.identity);
+            }
+
+            if(random == 3){
+                GameObject newEnemy = Instantiate(enemyType, spwn3.transform.position, Quaternion.identity);
+            }
+
+            if(random == 4){
+                GameObject newEnemy = Instantiate(enemyType, spwn4.transform.position, Quaternion.identity);
+            }
 
             zombieRunnerCount ++;
             GameManager.zombiesAlive ++;
@@ -60,10 +106,33 @@ public class EnemySpawner : MonoBehaviour
         while(zombieTankCount < baseZombieTankCount * GameManager.currentWave){
 
             yield return new WaitForSeconds(interval);
-            GameObject newEnemy = Instantiate(enemyType, transform.position, Quaternion.identity);
+            
+            if(random == 1){
+                GameObject newEnemy = Instantiate(enemyType, spwn1.transform.position, Quaternion.identity);
+            }
+                
+            if(random == 2){
+                GameObject newEnemy = Instantiate(enemyType, spwn2.transform.position, Quaternion.identity);
+            }
+
+            if(random == 3){
+                GameObject newEnemy = Instantiate(enemyType, spwn3.transform.position, Quaternion.identity);
+            }
+
+            if(random == 4){
+                GameObject newEnemy = Instantiate(enemyType, spwn4.transform.position, Quaternion.identity);
+            }
 
             zombieTankCount ++;
             GameManager.zombiesAlive ++;
         }
+    }
+
+    // resetta i contatori per la prossima ondata
+    public void ResetValues(){
+
+        zombieWalkerCount = 0;
+        zombieRunnerCount = 0;
+        zombieTankCount = 0;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,7 +12,6 @@ public class GameManager : MonoBehaviour
     public static int currentWave;
     public static int zombiesAlive;
 
-
     // Start is called before the first frame update
     void Start()
     {
