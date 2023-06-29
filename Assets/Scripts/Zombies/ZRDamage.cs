8645236c52daf9d1363c5073d727154d8affa673
Daniele Lurani <47564259+danielelurani@users.noreply.github.com>
2023-06-29 22:17:47 +0200

    Fix bug #8
    il danno degli zombie al giocatore non viene sempre triggerato

--- Assets/Scripts/Zombies/ZRDamage.cs
+++ Assets/Scripts/Zombies/ZRDamage.cs
@@ -5,19 +5,53 @@ using UnityEngine;
 public class ZRDamage : MonoBehaviour
 {
     [SerializeField] private int damage = 30;
+    [SerializeField] private float collisionRadius = 0.15f;
 
     private GameObject player;
     private CharacterStats playerStats;
+    private Animator animator;
+    private bool collisionProcessed = false;
 
     void Start(){
 
         // prendo l'oggetto player e il suo componente CharacterStats per poi usarlo nella funzione OnTriggerEnter
         player = GameObject.Find("Player");
         playerStats = player.GetComponent<CharacterStats>();
+
+        animator = GetComponentInParent<Animator>();
     }
 
-    private void OnTriggerEnter(Collider other) {
+    private void Update()
+    {
+        if (!collisionProcessed && animator.GetBool("Attack") && CheckCollision(transform.position, player.transform.position, collisionRadius))
+        {
+            playerStats.TakeDamage(damage);
+            collisionProcessed = true;
+            StartCoroutine(WaitForNextAttack());
+        }
+    }
+
+    bool CheckCollision(Vector3 thisObjPosition, Vector3 otherObjPosition, float radius)
+    {
+        Collider[] colliders = Physics.OverlapSphere(thisObjPosition, radius);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.gameObject == player)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private IEnumerator WaitForNextAttack(){
+
+        yield return new WaitForSeconds(2f);
+        collisionProcessed = false;
+    }
+
+    /*private void OnTriggerEnter(Collider other) {
         if(other.gameObject.tag == "Player")
             playerStats.TakeDamage(damage);
-    }
+    }*/
 }
