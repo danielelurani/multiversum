b81f7acca40f73f66a6a81553d10b4f4500b72be
Daniele Lurani <47564259+danielelurani@users.noreply.github.com>
2023-07-25 23:05:46 +0200

    Implementato cheat munizioni infinite

--- Assets/Scripts/Game/Cheats.cs
+++ Assets/Scripts/Game/Cheats.cs
@@ -7,16 +7,28 @@ public class Cheats : MonoBehaviour
 {
 
     private Toggle[] toggles;
+
     private GameObject player;
+    private Pistol pistol;
+    private Rifle rifle;
+    private Shotgun shotgun;
+    private EquippingScript equip;
     private CharacterStats playerStats;
 
     private bool immortalityActive;
+    private bool infiniteAmmoActive;
 
     void Start() {
         
         player = GameObject.Find("Player");
         playerStats = player.GetComponent<CharacterStats>();
+        pistol = player.GetComponentInChildren<Pistol>(true);
+        rifle = player.GetComponentInChildren<Rifle>(true);
+        shotgun = player.GetComponentInChildren<Shotgun>(true);
+        equip = player.GetComponent<EquippingScript>();
+
         immortalityActive = false;
+        infiniteAmmoActive = false;
     }
 
     // Update is called once per frame
@@ -24,8 +36,22 @@ public class Cheats : MonoBehaviour
     {
         toggles = GetComponentsInChildren<Toggle>(true);
 
+        // se il cheat è attivo, si avrà sempre il massimo della vita
         if(immortalityActive == true)
             playerStats.health = 100;
+
+        // se il cheat è attivo, si avrà sempre il massimo delle munizioni
+        if(infiniteAmmoActive == true){
+
+            if(equip.isSlot1Active)
+                pistol.currentBullets = 12;
+
+            if(equip.isSlot2Active)
+                rifle.currentBullets = 30;
+
+            if(equip.isSlot3Active)
+                shotgun.currentBullets = 8;
+        }
 
     }
 
@@ -43,6 +69,21 @@ public class Cheats : MonoBehaviour
                     playerStats.health = 100;
                     immortalityActive = false;
                 }
+            }
+        }
+    }
+
+    public void ActiveInfiniteAmmo(){
+
+        foreach(Toggle toggle in toggles){
+
+            if(toggle.gameObject.name == "Ammo"){
+
+                if(toggle.isOn)
+                    infiniteAmmoActive = true;
+                
+                if(!toggle.isOn)
+                    infiniteAmmoActive = false;
             }
         }
     }
